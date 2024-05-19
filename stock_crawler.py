1d8d7ac16de8a2365c74b7bf05b8a027123782e7
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-05-19 23:55:33 +0700

    stock crawler

--- stock_crawler.py
+++ stock_crawler.py
@@ -1,9 +1,29 @@
-from vnstock3 import *
+import json
 import os
-
 
 if "ACCEPT_TC" not in os.environ:
     os.environ["ACCEPT_TC"] = "tôi đồng ý"
 
-stock = Vnstock().stock(symbol='MBS', source='VCI')
-print(stock.quote.history(start='2023-01-01', end='2024-01-31', interval='1D'))
+from vnstock3 import *
+
+
+def main():
+    stock_list = ["FPT", "VCB", "HPG", "VPB", "VNM", "VIC"]
+
+    with open("./data/stock.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    for i in stock_list:
+        stock = Vnstock().stock(symbol=i, source='VCI')
+        df = stock.quote.history(start='2023-01-01', end='2024-01-31', interval='1D')
+        df['time'] = df['time'].dt.strftime('%Y-%m-%d')
+        data[i] = df.to_dict(orient='list')
+
+    with open('./data/stock.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
